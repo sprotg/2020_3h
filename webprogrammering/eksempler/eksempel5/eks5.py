bf42a6f59a4519fdf5114e796396fae1bacbc0f5
sprotg <spr@sde.dk>
2019-10-09 07:46:12 +0200

    Eksempel 5

--- webprogrammering/eksempler/eksempel5/eks5.py
+++ webprogrammering/eksempler/eksempel5/eks5.py
@@ -27,5 +27,6 @@ if __name__ == "__main__":
     with app.app_context():
         with open('static/pizza_file.json', "r") as read_file:
             pizzaer = json.load(read_file)
+    print(pizzaer[0])
 
     app.run(debug=True)
