91ab3743a1eb02912b5b8235c62fcbb8765569b8
sprotg <spr@sde.dk>
2019-10-28 08:45:28 +0100

    Eksempel med billeder tilføjet

--- webprogrammering/eksempler/eksempel5/eks5.py
+++ webprogrammering/eksempler/eksempel5/eks5.py
@@ -15,12 +15,13 @@ def index():
     return render_template('index.html', pizzaer = pizzaer)
 
 
+
 def addPizza(name, price, description):
     pizzaer.append({'name' : name, 'price' : price, 'description' : description})
     savePizzaer()
 
 def savePizzaer():
-    with open("pizza_file.json", "w") as write_file:
+    with open("static/pizza_file.json", "w") as write_file:
         json.dump(pizzaer, write_file)
 
 if __name__ == "__main__":
